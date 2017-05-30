265324a76277625ba6370a884e563b72716cf3aa
benfasoli <benfasoli@gmail.com>
2017-05-30 14:32:56 -0600

    Added historic CO2 page

--- _render_air.r
+++ _render_air.r
@@ -71,16 +71,149 @@ cal <- lapply(sites, function(site) {
 }) %>%
   bind_rows()
 
-# RMSE timeseries plots --------------------------------------------------------
-# Subset calibrated dataset to produce recent data for RMSE timeseries plots
-n <- 24 * 7 # Hourly resolution
+# Concentration timeseries plots -----------------------------------------------
+# Produce concentration timeseries and diurnal cycles
 recent <- cal %>%
   rename(Time_Mountain = Time_UTC) %>%
-  filter(Time_Mountain >= Sys.time() - 7 * 24 * 3600) %>%
+  filter(Time_Mountain >= Sys.time() - 10 * 24 * 3600) %>%
+  group_by(site_id,
+           Time_Mountain = trunc(Time_Mountain, units = 'hours') %>% 
+             as.POSIXct()) %>%
+  summarize_each(funs(mean(., na.rm = T)), CO2d_ppm_cal, CH4d_ppm_cal) %>%
+  ungroup()
+
+f <- recent %>%
+  select(Time_Mountain, CO2d_ppm_cal, site_id) %>%
+  na.omit() %>%
+  ggplot(aes(x = Time_Mountain, y = CO2d_ppm_cal, color = site_id)) +
+  geom_line(alpha = 0.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/ts_co2.rds')
+
+f <- recent %>%
+  select(Time_Mountain, CH4d_ppm_cal, site_id) %>%
+  na.omit() %>%
+  ggplot(aes(x = Time_Mountain, y = CH4d_ppm_cal, color = site_id)) +
+  geom_line(alpha = 0.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/ts_ch4.rds')
+
+
+# Diurnal cycle plots ----------------------------------------------------------
+# Produce concentration timeseries and diurnal cycles
+diurnal <- recent %>%
+  group_by(site_id,
+           Hour = as.numeric(format(Time_Mountain, tz = 'America/Denver',
+                                    format = '%H'))) %>%
+  summarize_each(funs(mn = mean(., na.rm = T), sd = sd(., na.rm = T)),
+                 CO2d_ppm_cal, CH4d_ppm_cal) %>%
+  ungroup()
+
+f <- diurnal %>%
+  select(Hour, CO2d_ppm_cal_mn, site_id) %>%
+  na.omit() %>%
+  ggplot(aes(x = Hour, y = CO2d_ppm_cal_mn, color = site_id)) +
+  geom_line(alpha = 0.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  coord_cartesian(ylim = c(380, 500)) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/diurnal_co2.rds')
+
+f <- diurnal %>%
+  select(Hour, CH4d_ppm_cal_mn, site_id) %>%
+  na.omit() %>%
+  ggplot(aes(x = Hour, y = CH4d_ppm_cal_mn, color = site_id)) +
+  geom_line(alpha = 0.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  coord_cartesian(ylim = c(1.85, 2.85)) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/diurnal_ch4.rds')
+
+f <- diurnal %>%
+  select(Hour, CO2d_ppm_cal_sd, site_id) %>%
+  na.omit() %>%
+  ggplot(aes(x = Hour, y = CO2d_ppm_cal_sd, color = site_id)) +
+  geom_line(alpha = 0.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  coord_cartesian(ylim = c(0, 60)) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/diurnal_co2_sd.rds')
+
+f <- diurnal %>%
+  select(Hour, CH4d_ppm_cal_sd, site_id) %>%
+  na.omit() %>%
+  ggplot(aes(x = Hour, y = CH4d_ppm_cal_sd, color = site_id)) +
+  geom_line(alpha = 0.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  coord_cartesian(ylim = c(0, 1)) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/diurnal_ch4_sd.rds')
+
+# Pre-calibration bias plots ---------------------------------------------------
+# Subset parsed dataset to produce recent data for calibration offset plots
+recent <- parsed %>%
+  rename(Time_Mountain = Time_UTC) %>%
+  filter(Time_Mountain >= Sys.time() - 7 * 24 * 3600,
+         ID_co2 > 0) %>%
+  mutate(dCO2d_ppm = CO2d_ppm - ID_co2,
+         dCH4d_ppm = CH4d_ppm - ID_ch4) %>%
+  group_by(site_id,
+           Time_Mountain = trunc(Time_Mountain, units = 'hours') %>% 
+             as.POSIXct()) %>%
+  summarize_each(funs(mean(., na.rm = T)), dCO2d_ppm, dCH4d_ppm)
+
+f <- ggplot(data = recent,
+            aes(x = Time_Mountain, y = dCO2d_ppm, color = site_id)) +
+  geom_line() +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/cal_offset_co2.rds')
+
+f <- ggplot(data = recent %>% na.omit(),
+            aes(x = Time_Mountain, y = dCH4d_ppm, color = site_id)) +
+  geom_line() +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        text = element_text(size = 10))
+saveRDS(f, 'data/cal_offset_ch4.rds')
+
+
+# RMSE timeseries plots --------------------------------------------------------
+# Subset calibrated dataset to produce recent data for RMSE timeseries plots
+# n <- 24 * 7 # Hourly resolution
+recent <- cal %>%
+  rename(Time_Mountain = Time_UTC) %>%
+  filter(Time_Mountain >= Sys.time() - 10 * 24 * 3600) %>%
   group_by(site_id,
            Time_Mountain = trunc(Time_Mountain, units = 'hours') %>% 
              as.POSIXct()) %>%
   summarize_each(funs(mean(., na.rm = T)), rmse_co2, rmse_ch4) %>%
+  ungroup() %>%
   mutate(rmse_ch4 = rmse_ch4 * 1000)
 
 max_rmse <- 2.0
@@ -126,40 +259,6 @@ f <- ggplot(data = recent %>% na.omit(),
         text = element_text(size = 10))
 saveRDS(f, 'data/rmse_ch4.rds')
 
-
-# Pre-calibration bias plots ---------------------------------------------------
-# Subset parsed dataset to produce recent data for calibration offset plots
-# n <- 24 * 7 # Hourly resolution
-recent <- parsed %>%
-  rename(Time_Mountain = Time_UTC) %>%
-  filter(Time_Mountain >= Sys.time() - 7 * 24 * 3600,
-         ID_co2 > 0) %>%
-  mutate(dCO2d_ppm = CO2d_ppm - ID_co2,
-         dCH4d_ppm = CH4d_ppm - ID_ch4) %>%
-  group_by(site_id,
-           Time_Mountain = trunc(Time_Mountain, units = 'hours') %>% 
-             as.POSIXct()) %>%
-  summarize_each(funs(mean(., na.rm = T)), dCO2d_ppm, dCH4d_ppm)
-
-f <- ggplot(data = recent,
-            aes(x = Time_Mountain, y = dCO2d_ppm, color = site_id)) +
-  geom_line() +
-  xlab(NULL) +
-  ylab(NULL) +
-  theme_classic() +
-  theme(legend.title = element_blank(),
-        text = element_text(size = 10))
-saveRDS(f, 'data/cal_offset_co2.rds')
-
-f <- ggplot(data = recent %>% na.omit(),
-            aes(x = Time_Mountain, y = dCH4d_ppm, color = site_id)) +
-  geom_line() +
-  xlab(NULL) +
-  ylab(NULL) +
-  theme_classic() +
-  theme(legend.title = element_blank(),
-        text = element_text(size = 10))
-saveRDS(f, 'data/cal_offset_ch4.rds')
 
 
 # Compute site summary statistics ----------------------------------------------
@@ -247,6 +346,7 @@ saveRDS(result, 'data/stats.rds')
 
 # Compile webpage --------------------------------------------------------------
 render_air <- function() {
+  # Status page ----------------------------------------------------------------
   tmp_path <- render(input = './_dash_src.Rmd',
                      output_file = './.tmp.html',
                      output_format = flex_dashboard(
@@ -255,10 +355,7 @@ render_air <- function() {
                        vertical_layout = 'scroll',
                        includes = includes(
                          in_header = c('_header.html',
-                                       '_header_refresher.html')
-                       )
-                     ))
-  
+                                       '_header_refresher.html'))))
   html <- read_lines(tmp_path)
   system(paste('rm', tmp_path))
   nav  <- read_lines('_navbar.html')
@@ -271,7 +368,31 @@ render_air <- function() {
   out <- append(out, html[(delete[2]+1):length(html)])
   write_lines(out, 'status.html')
   
+  # Historic CO2 page ----------------------------------------------------------
+  tmp_path <- render(input = './_historic_co2_src.Rmd',
+                     output_file = './.tmp.html',
+                     output_format = flex_dashboard(
+                       css = 'styles.css',
+                       orientation = 'rows',
+                       vertical_layout = 'scroll',
+                       includes = includes(
+                         in_header = '_header.html')))
+  html <- read_lines(tmp_path)
+  system(paste('rm', tmp_path))
+  nav  <- read_lines('_navbar.html')
+  delete <- c(
+    grep('<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">', 
+         html, fixed=T),
+    grep('</div><!--/.navbar-->', html))
+  out <- html[1:(delete[1]-1)]
+  out <- append(out, nav)
+  out <- append(out, html[(delete[2]+1):length(html)])
+  write_lines(out, 'historic_co2.html')
+  
+  # Render site and clean up ---------------------------------------------------
   rmarkdown::render_site(encoding = 'UTF-8')
+  
+  system('rm historic_co2.html')
   system('rm status.html')
   system(paste('cp -R _site/* /var/www/air.utah.edu/'))
 }
